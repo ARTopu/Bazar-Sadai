5779315aa9111a49430fed24fb9120c611383316
Atiqur Rahman <atiqurrahman2312002@gmail.com>
2023-11-13 21:42:24 +0600

    new update of data

--- server/src/data.js
+++ server/src/data.js
@@ -15,6 +15,13 @@ const data = {
         address: "Mannat",
     },
     {
+        name: "Jhon Cena",
+        email: "Jhon@gmail.com",
+        password: "98765",
+        phone: "0179876543",
+        address: "Vhopal",
+    },
+    {
         name: "Tasnim Hasan",
         email: "tasnim@gmail.com",
         password: "54321",
