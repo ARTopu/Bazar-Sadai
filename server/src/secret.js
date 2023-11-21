d3425175a8cfdfa465a8612b6daf7351718f24d2
Atiqur Rahman <atiqurrahman2312002@gmail.com>
2023-11-21 13:11:08 +0600

    created JWT Key

--- server/src/secret.js
+++ server/src/secret.js
@@ -3,5 +3,6 @@ require('dotenv').config();
 const serverPort = process.env.SERVER_PORT || 3002;
 const mongodbURL = process.env.MONGODB_ATLAS_URL || "mongodb://localhost:27017/BazarSadaiDB";
 const defaultImagePath = process.env.DEFAULT_USER_IMAGE_PATH;
+const jwtActivationKey = process.env.JWT_ACTIVATION_KEY || 'sd5lkf9jsd3fn45lks4fdsl6dk4';
 
-module.exports = {serverPort, mongodbURL,defaultImagePath};
+module.exports = {serverPort, mongodbURL,defaultImagePath,jwtActivationKey};
